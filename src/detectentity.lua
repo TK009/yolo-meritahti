75485e4beb1530ef1e28bcbcbe07205049e9eeb2
Ahti Katiska <ahti.katiska@helsinki.fi>
2014-12-25 23:58:47 +0200

    Added the need of fuel to the detectentity

--- src/detectentity.lua
+++ src/detectentity.lua
@@ -3,6 +3,10 @@ if (turtle.detect()) then
     return false
 end
 
+if(turtle.getFuelLevel = 0) then
+    error("Fuel is required to check entity presence!")
+end
+
 local canmove = turtle.forward()
 
 if canmove then
