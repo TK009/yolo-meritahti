d50c94e3ccbf52bfcbfbf699281a773d95ad867f
TK009 <TK009@users.noreply.github.com>
2014-12-25 23:01:23 +0200

    fix error in detect and wrap in a function

--- src/detectentity.lua
+++ src/detectentity.lua
@@ -1,12 +1,13 @@
-
-if (turtle.detect()) then
-    return false
+function detectEntity()
+    if (turtle.detect()) then
+        return false
+    end
+    
+    local canmove = turtle.forward()
+    
+    if canmove then
+        turtle.back()
+    end
+    
+    return not canmove
 end
-
-local canmove = turtle.forward()
-
-if canmove then
-    turtle.back()
-end
-
-return canmove
